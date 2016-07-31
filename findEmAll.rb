e5963f3252c8e73909633439494e9b02d8c17276
Aaron Lunsford <lunsford@webassign.net>
2016-07-30 20:24:34 -0400

    Clear database on env var

--- findEmAll.rb
+++ findEmAll.rb
@@ -33,9 +33,9 @@ locations.each_with_index do |(place, location), index|
 
   # Tack on arguments to the runserver.py command.
   # Need just 1 web server, so only the last location starts one up. Tack on the
-  # -cd flag to clear the database when the first server starts.
+  # -cd flag if the CD env var is set to true.
   cmd << " -ns"        if index != 0
-  cmd << " -cd"        if index == 0 && ARGV[0] == "-cd"
+  cmd << " -cd"        if index == 0 && ENV['CD']
   cmd << " -H 0.0.0.0" if index == 0
 
   # Uncomment to suppress python output
